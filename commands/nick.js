9c9489e283275341c2d9a1b7e17fbe2eeeede3f0
Velkan <gbenitez@velkan-vault.com>
2020-09-07 23:04:49 -0500

    Fixed bug when the nickname or channel name has spaces, Fixed bug no updating user nickname when in channel, Fixed bug can't change nickname when there are channels with same name (added return), Fixed bug no updating nicknames on new channels

--- commands/nick.js
+++ commands/nick.js
@@ -6,6 +6,25 @@ const nameTracker = trackerPath + nameTrackerJSON;
 
 module.exports = {
     execute(msg, args) {
+        //If there are more than 4 arguments means that args separate spaces wrongly or is admin assign to other target.
+        //In either case, lets check for wrong spacing
+        if(args.length > 4){
+            var arguments = [];
+            for(let i = 2; i < args.length;i++){
+                arguments.push(args[i]);
+            }
+            arguments = arguments.join(" ");
+            //assign random substring to separate correctly
+            arguments = arguments.replace('" "','"~°|°~"');
+            arguments = arguments.replace('" <','"~°|°~<');
+            arguments = arguments.split(/~°\|°~/);
+            //rearrange args with new separations
+            let nargs = [args[0],args[1]];
+            for(let i = 0; i < arguments.length; i++){
+                nargs.push(arguments[i]);
+            }
+            args = nargs;
+        }
 
         //Set message sender user as default user
         let userID = msg.author.id;
@@ -66,13 +85,16 @@ module.exports = {
         }
 
         //validateChannel & search ChannelID
-        channel = msg.guild.channels.cache.find(channel => channel.name === channelName);
+        channel = msg.guild.channels.cache.filter((channel)=>{ return channel.name === channelName && channel.type === 'voice'; });
         var channelID = "";
-        if(!channel){
+        if(channel.size == 0){
             util.print(msg,'',"There is no such channel. Maybe you made a typo?",'red');
             return;
+        }else if(channel.size != 1){
+            util.print(msg,'',`I have found ${channel.size} channels with the same name.\nPlease rename the channels so I can select the correct one.`,'red');
+            return;
         }else{
-            channelID = channel.id;
+            channelID = channel.first().id;
         }
 
         //Load nicknamesTracker File
@@ -92,7 +114,7 @@ module.exports = {
                 //Checks if player exists, otherwise creates it
                 if(!(userID in nickJSON.players)){
                     nickJSON.players[userID] = {
-                        name: msg.member.displayName,
+                        name: msg.guild.member(userID).user.username,
                         userid: userID,
                         registrations: {}
                     }
@@ -110,7 +132,7 @@ module.exports = {
 
                 //Confirmation
                 util.print(msg,'',`Excellent!\nI have saved <@${userID}>'s new nickname "${nickName}" to the channel "${channelName}"`,'green');
-
+                checkNicknames(msg,channelID,userID);
                 break;
             case 'delete':
                 //Checks if the player exists or has nicknames
@@ -135,7 +157,12 @@ module.exports = {
                     util.print(msg,'',`I have removed <@${userID}>'s nickname "${nickName}" from the channel "${channelName}"`,'green');
                 }
 
+                checkNicknames(msg,channelID,userID);
+
                 break;
+            case 'test':
+                checkNicknames(msg,channelID,userID);
+            break;
             default:
                 util.print(msg,'',`Sorry <@${msg.author.id}> it looks like you have forgotten something. Try this way:\n${p}nick <register|delete> \"<New_nickname>\" \"<Channel_Name>\"`,'red');
             break;
@@ -151,7 +178,7 @@ module.exports = {
 		
         //Check if userid is in players
         if (!(newState.member.user.id in nickJSON.players)) return;
-
+        
         //Retrive nickname
         var nickname = nickJSON.players[newState.member.user.id].registrations[newState.channelID];
         
@@ -180,4 +207,17 @@ module.exports.help = {
 function saveNick(fts) {
     fs.writeFileSync(nameTracker, JSON.stringify(fts), null, 4);
     console.log("Succesfully saved to [" + nameTrackerJSON + "]!")
+}
+
+function checkNicknames(msg,channelID,userID){
+    //get member to check
+    var member = msg.guild.members.cache.find(user => user.id === userID);
+    //if is in updated voice channel set new nickname
+    if(member.voice.channel != null && member.voice.channel.id == channelID){
+        var nickJSON = fs.readFileSync(nameTracker);
+        nickJSON = JSON.parse(nickJSON);
+        var nickname = nickJSON.players[userID].registrations[channelID];
+        if(nickname == undefined || nickname == ''){ nickname = nickJSON.players[userID].name; }
+        member.setNickname(nickname);
+    }
 }
